234fd47f1c3509a99a904aa6a1983c895455fbf4
GarrafaTati <tatiigarrafa@gmail.com>
2023-06-06 12:20:34 -0400

    using useReducer

--- Exercise Files/Ch04/04_07/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_07/start/react-app/src/App.js
@@ -1,38 +1,27 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+// import { useState } from "react";
+import { useReducer } from "react";
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] =
-    useState("tired");
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`);
-  }, [emotion]);
 
-  useEffect(() => {
-    console.log(`It's ${secondary} around here!`);
-  }, [secondary]);
+  //better way to manage the checkbox
+  const [checked, setChecked] = useReducer(checked => !checked, false);
+  
   return (
     <div className="App">
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("sad")}>
-        Sad
-      </button>
-      <button
-        onClick={() => setEmotion("excited")}
-      >
-        Excited
-      </button>
-      <h2>
-        Current secondary emotion is {secondary}.
-      </h2>
-      <button
-        onClick={() => setSecondary("grateful")}
-      >
-        Grateful
-      </button>
+      <input type="checkbox" value={checked} onChange={setChecked} />
+      <label>{checked ? "checked" : "not checked"}</label>
     </div>
-  );
+  )
+  // it's ok to do this way
+  // const [checked, setChecked] = useState(false);
+  
+  // return (
+  //   <div className="App">
+  //     <input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)} />
+  //     <label>{checked ? "checked" : "not checked"}</label>
+  //   </div>
+  // );
 }
 
 export default App;
