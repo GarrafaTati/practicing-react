444cd6097b18a84881ce9fe4a50b707e7b2f934f
GarrafaTati <tatiigarrafa@gmail.com>
2023-06-16 11:45:35 -0400

    displaying data from an API

--- Exercise Files/Ch06/06_02/start/react-app/src/App.js
+++ Exercise Files/Ch06/06_02/start/react-app/src/App.js
@@ -1,19 +1,24 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
+function GitHubUser({ name, location, avatar }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} alt={name} height={150} />
+    </div>
+  );
+}
+
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch(
-      `https://api.github.com/users/moonhighway`
-    )
+    fetch(`https://api.github.com/users/GarrafaTati`)
       .then((response) => response.json())
       .then(setData);
   }, []);
-  if (data)
-    return (
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    );
+  if (data) return <GitHubUser name={data.name} location={data.location} avatar={data.avatar_url} />;
   return <h1>Data</h1>;
 }
 
